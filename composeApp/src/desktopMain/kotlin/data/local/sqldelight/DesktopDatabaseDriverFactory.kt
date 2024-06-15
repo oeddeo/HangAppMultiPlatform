dc482d272308d8607a6dee85c53e83b2c6211a5d
oeddeo <104972668+oeddeo@users.noreply.github.com>
2024-06-15 18:00:49 +0200

    fix: persistent db works too

--- composeApp/src/desktopMain/kotlin/data/local/sqldelight/DesktopDatabaseDriverFactory.kt
+++ composeApp/src/desktopMain/kotlin/data/local/sqldelight/DesktopDatabaseDriverFactory.kt
@@ -4,33 +4,27 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.kmpktor.AppDatabase
 import java.io.File
-//
-//class DesktopDatabaseDriverFactory : DataBaseDriverFactory {
-//    override fun createDriver(): SqlDriver {
-//        val dbFilePath: String = getPath(isDebug = false)
-//        val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:$dbFilePath")
-//
-//        if (!File(dbFilePath).exists()) {
-//            AppDatabase.Schema.create(driver)
-//        }
-//        return driver
-//    }
-//
-//    private fun getPath(isDebug: Boolean): String {
-//        val propertyKey = if (isDebug) "java.io.tmpdir" else "user.home"
-//        val parentFolderPath = System.getProperty(propertyKey) + "/SqlDelightDemo"
-//        val parentFolder = File(parentFolderPath)
-//        if (!parentFolder.exists()) {
-//            parentFolder.mkdirs()
-//        }
-//        val databasePath = File(parentFolderPath, "ktorapp.db")
-//        return databasePath.absolutePath
-//    }
-//}
+
 class DesktopDatabaseDriverFactory : DataBaseDriverFactory {
     override fun createDriver(): SqlDriver {
-        val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-        AppDatabase.Schema.create(driver)
+        val dbFilePath: String = getPath(isDebug = false)
+        val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:$dbFilePath")
+
+        if (!File(dbFilePath).exists()) {
+            AppDatabase.Schema.create(driver)
+        }
         return driver
     }
+
+    private fun getPath(isDebug: Boolean): String {
+        val propertyKey = if (isDebug) "java.io.tmpdir" else "user.home"
+        val parentFolderPath = System.getProperty(propertyKey) + "/SqlDelightDemo"
+        val parentFolder = File(parentFolderPath)
+        if (!parentFolder.exists()) {
+            parentFolder.mkdirs()
+        }
+        val databasePath = File(parentFolderPath, "ktorapp.db")
+        return databasePath.absolutePath
+    }
 }
+
